13ae70d2c1cca0023544877058476865c6eb9a6d
Abdulrahman-Bajaman <Abdulrahman.Abdullah@jisr.net>
2025-07-11 18:49:45 +0300

    Rails strucure

---
+++ db/migrate/20250711135745_create_school_table.rb
@@ -0,0 +1,14 @@
+class CreateSchoolTable < ActiveRecord::Migration[8.0]
+  def change
+    create_table :schools do |t|
+      t.string :name_en, limit: 100, null: false
+      t.string :name_ar, limit: 100, null: false
+      t.string :address
+      t.boolean :status, null: false
+      t.timestamps
+    end
+
+    add_index :schools, :name_en, unique: true
+    add_index :schools, :name_ar, unique: true
+  end
+end
